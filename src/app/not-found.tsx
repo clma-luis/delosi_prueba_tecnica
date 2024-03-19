334dbc60f7f740141f2efb6e4b79479b36cea6e0
Carlos Luis <marincarlosl19@gmail.com>
2024-03-19 14:09:46 -0500

    [FEAT]: manejando las paginas que no existen

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/Footer";
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
+
+const notFound = () => {
+  return (
+    <>
+      <div className="flex flex-col h-screen w-full items-center justify-center px-4 text-[18px] font-bold">
+        <h1 className="font-custom text-center">¡Oh, no! Esta página no existe, vuelve a la página principal.</h1>
+        <Link href="/">
+          <Button className="mt-4">VOLVER</Button>
+        </Link>
+      </div>
+      <div className="fixed bottom-0 left-0 flex h-16 w-full items-end justify-center">
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default notFound;

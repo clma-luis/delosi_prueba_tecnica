3e3ce7873384a9a0c7a4f50fdba991fc5780709a
Carlos Luis <marincarlosl19@gmail.com>
2024-03-19 11:31:14 -0500

    [FIX]: agregando la etiqueta footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,13 +2,13 @@
 
 const Footer = () => {
   return (
-    <div
+    <footer
       className={`flex  "pr-0 justify-center items-center fixed bottom-0 left-0 z-2 w-[100%]  mb-2 bg-popover dark:bg-popover dark:border-secondary`}
     >
       <p className=" font-custom text-[14px] pr-2">Powered by</p>
 
       <span className="self-center text-[18PX] font-bold whitespace-nowrap dark:text-white">CARLOS MARIN</span>
-    </div>
+    </footer>
   );
 };
 

c8b35d361743ca8960c38d0edbee5e021c01275d
Carlos Luis <marincarlosl19@gmail.com>
2024-03-18 23:22:37 -0500

    [FEAT]: MEJORANDO LAS FUNCIONALIDADES

--- src/modules/HomeModules/index.tsx
+++ src/modules/HomeModules/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { InputOtp } from "@/components/InputOpt";
 import { Button } from "@/components/ui/Button";
 import { Card } from "@/components/ui/Card";
@@ -10,19 +9,18 @@ import { initialMatrixState } from "./components/SelectOptions/config";
 
 const HomeModules = () => {
   const [matrix, setMatrix] = useState<string[][]>(initialMatrixState);
-  const [matrixRotated, setMatrixRotated] = useState<string[][] | []>([]);
   const [selectedValue, setSelectedValue] = useState("3");
   const [disableBtn, setDisableBtn] = useState(false);
 
-  const handleRotateMatrix = () => {
-    const rotatedMatrix = rotateToTheLeft(matrix);
-    setMatrix(rotatedMatrix);
+  const handleRotateMatrix = (rotateAll?: boolean) => {
+    const rotatedMatrix = rotateToTheLeft(matrix, rotateAll);
+    setMatrix(rotatedMatrix as string[][]);
   };
 
   const handleAutoFill = () => {
-   const value = autoFillMatrix(Number(selectedValue));
-   setMatrix(value)
-  }
+    const value = autoFillMatrix(Number(selectedValue));
+    setMatrix(value);
+  };
 
   const handleOnChangeValue = (value: string) => {
     setSelectedValue(value);
@@ -38,14 +36,26 @@ const HomeModules = () => {
     }
   };
 
+  const handleDisableBtn = () => {
+    const value = matrix.flat().some((el) => !el);
+
+    return value || disableBtn;
+  };
+
   return (
     <>
       <div className="max-w-screen-xl flex flex-col mx-auto p-4">
-
         <div className="w-full mb-4 grid grid-cols-1 md:grid-cols-3 gap-2">
           <SelectOptions selectedValue={selectedValue} handleOnChangeValue={handleOnChangeValue} handleOnChangeOpen={handleOnChangeOpen} />
-          <Button disabled={disableBtn} onClick={handleRotateMatrix}>ROTAR</Button>
-          <Button disabled={disableBtn} onClick={handleAutoFill}>AUTO RELLENAR</Button>
+          <Button disabled={disableBtn} onClick={handleAutoFill}>
+            AUTO RELLENAR
+          </Button>
+          <Button disabled={handleDisableBtn()} onClick={() => handleRotateMatrix()}>
+            ROTAR SOLO LOS BORDES
+          </Button>
+          <Button disabled={handleDisableBtn()} onClick={() => handleRotateMatrix(true)}>
+            ROTAR TODO
+          </Button>
         </div>
 
         <Card className="w-full p-4  flex flex-col justify-center items-center">
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -5,13 +5,19 @@ export const generateInitialMatrix = (length: number): string[][] | [] => {
   return value;
 };
 
-export const rotateToTheLeft = (matrix: string[][]) => {
+export const rotateToTheLeft = (matrix: string[][], rotateAll: boolean = false) => {
   const matrixLength = matrix.length;
   const currentMatrix = generateInitialMatrix(matrixLength) as string[][];
 
   for (let i = 0; i < matrixLength; i++) {
     for (let j = 0; j < matrixLength; j++) {
-      currentMatrix[matrixLength - 1 - j][i] = matrix[i][j];
+      const currentLength = matrixLength - 1;
+      const rest = currentLength - j;
+      if (i > 0 && i < currentLength && j > 0 && j < currentLength && !rotateAll) {
+        currentMatrix[i][j] = matrix[i][j];
+      } else {
+        currentMatrix[rest][i] = matrix[i][j];
+      }
     }
   }
 
@@ -24,9 +30,10 @@ export const autoFillMatrix = (matrixLength: number) => {
   const value = matrix.reduce(
     (acc: { matrix: string[][]; lastValue: number }, el) => {
       const newElement = el.map((_) => {
+        const value = acc.lastValue + 1;
         acc.lastValue = acc.lastValue + 1;
 
-        return String(acc.lastValue + 1);
+        return String(value);
       });
 
       return { matrix: [...acc.matrix, newElement], lastValue: acc.lastValue };
